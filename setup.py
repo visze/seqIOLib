44e6a16e35f68fdbf47cf51b43c55c2b185689d5
Max <visze@users.noreply.github.com>
2022-04-13 14:34:53 +0200

    bumping numpy

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ["pyfaidx", "numpy<1.19.0", "tensorflow==2.5.3"]
+requirements = ["pyfaidx", "numpy>=1.21", "tensorflow==2.5.3"]
 
 setup_requirements = ['pytest-runner', ]
 
